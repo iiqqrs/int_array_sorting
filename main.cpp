ea84198386fdb8e679dd4aa320a1fadf09e1f537
Jesse <jrinaldi@udel.edu>
2016-03-22 03:40:24 +0000

    Finished shell sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "shell_sort.cpp"
+#include <ctime>
 using namespace std;
 
-int main() {
+void populate(int *a, int size){
+    for(int i = 0; i < size; i++){
+        *(a + i) = rand() % 101;
+    }
+}
+
+int main(){
+    //Set up time stuff
+    clock_t start;
+    double duration;
+    int input_array_size;
     
+    cout << "Enter an array size for shell sort." <<endl;
+    cin >> input_array_size;
+    int array_to_sort[input_array_size];
+    int* p;
+    p = array_to_sort;
+    populate(p, input_array_size);
+    
+    /*
+    for(int i : array_to_sort){
+        cout << " " << i <<endl;
+    }
+    */
+    
+    //Shell Sort test.
+    start = clock();
+    shell_sort(p, input_array_size);
+    duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout << "It took " << duration << " seconds to shell sort an array of ints of size " << input_array_size <<endl;
 }
--- shell_sort.cpp
+++ shell_sort.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
+
 //Shell sort
+void shell_sort(int* a, int size){
+    int increment;
+    int i;
+    int j;
+    //int size = sizeof(a)/sizeof(int); // ASSUMING WE'RE ONLY USING INTS.
+    //cout << "size: " << sizeof(a) <<endl;
+    int buffer;
+    for(increment = size/2; increment > 0; increment /= 2){
+        for(i = increment; i<size; i++){
+            buffer = *(a + i); //a[i];
+            for(j = i; j >= increment; j-=increment){
+                if(buffer < *(a + (j-increment))){
+                    *(a + j) = *(a + (j-increment));
+                }else{
+                    break;
+                }
+            }
+            *(a + j) = buffer;
+        }
+    }
+}
